713f81ffbd9b7191f2d1e2004e38b26d6edfc24d
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-09-06 17:01:38 -0300

    feat: refactor event fetching logic and remove commented-out code

--- src/prbr25_startgg_queries/get_events.py
+++ src/prbr25_startgg_queries/get_events.py
@@ -1,8 +1,14 @@
-from datetime import datetime
+import pandas as pd
 
 from prbr25_startgg_queries.common.config import STARTGG_BEARER_TOKEN, events_dict
 from prbr25_startgg_queries.common.logger import setup_logger
 from prbr25_startgg_queries.extract.graphql import GraphQL
+from prbr25_startgg_queries.transform.clean_events import (
+    clean_event_and_phases_dataframes,
+)
+from prbr25_startgg_queries.transform.extract_event_phases import (
+    extract_phase_and_event_from_response,
+)
 
 logger = setup_logger(__name__)
 
@@ -13,10 +19,22 @@ def request_events(start_date: int, end_date: int):
     raw_tournament_data = GraphQL(STARTGG_BEARER_TOKEN).query_all_pages(
         "events", events_dict
     )
-    print(raw_tournament_data)
+    return raw_tournament_data
 
 
-end_date = datetime.now()
-start_date = datetime(end_date.year, 1, 1)
-# start_date = end_date - timedelta(weeks=1)
-request_events(int(start_date.timestamp()), int(end_date.timestamp()))
+def get_events_and_phases(start_timestamp: int, end_timestamp: int):
+    data = request_events(start_timestamp, end_timestamp)
+    event_list, phase_list = extract_phase_and_event_from_response(data)
+    event_df = pd.DataFrame(event_list)
+    phase_df = pd.DataFrame(phase_list)
+    return clean_event_and_phases_dataframes(event_df, phase_df)
+
+
+# end_date = datetime.now()
+# # start_date = end_date - timedelta(weeks=4)
+# start_date = datetime(end_date.year, 1, 1)
+# start_timestamp = int(start_date.timestamp())
+# end_timestamp = int(end_date.timestamp())
+# event_df, phase_df = get_events_and_phases(start_timestamp, end_timestamp)
+# event_df.to_csv("test_events.csv")
+# phase_df.to_csv("test_phases.csv")

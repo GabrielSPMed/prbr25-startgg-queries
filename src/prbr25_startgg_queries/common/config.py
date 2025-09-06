540c166a733222dcf9e25aaf0b59ee375b88de0f
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-09-06 17:01:55 -0300

    feat: add event_table_columns and phase_table_columns for tournament data

--- src/prbr25_startgg_queries/common/config.py
+++ src/prbr25_startgg_queries/common/config.py
@@ -15,3 +15,19 @@ events_dict = {
     "afterdate": 1735700400,
     "beforedate": 1755494257,
 }
+
+event_table_columns = [
+    "tournament_id",
+    "tournament_name",
+    "address_state",
+    "city",
+    "url",
+    "id",
+    "event_state",
+    "event_name",
+    "num_entrants",
+    "start_at",
+    "last_update_at	event_value",
+]
+
+phase_table_columns = ["id", "name", "bracket_type", "event_id"]

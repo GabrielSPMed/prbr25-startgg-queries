c6aaab695c889ef1c3d1c129181775522aae4654
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-09-20 23:30:30 -0300

    refactor: simplify refresh_raw_events function and remove unused imports

--- src/prbr25_startgg_queries/entrypoint.py
+++ src/prbr25_startgg_queries/entrypoint.py
@@ -1,7 +1,6 @@
-from datetime import datetime
-
 import pandas as pd
 
+from prbr25_startgg_queries.bd.postgres import Postgres
 from prbr25_startgg_queries.common.config import (
     POSTGRES_DB,
     POSTGRES_HOST,
@@ -10,8 +9,8 @@ from prbr25_startgg_queries.common.config import (
     POSTGRES_USERNAME,
 )
 from prbr25_startgg_queries.common.logger import setup_logger
+from prbr25_startgg_queries.extract.refresh_raw import get_timestamps_for_refresh
 from prbr25_startgg_queries.extract.utils import request_events
-from prbr25_startgg_queries.load.postgres import Postgres
 from prbr25_startgg_queries.transform.clean_events import (
     clean_event_and_phases_dataframes,
 )
@@ -22,19 +21,23 @@ from prbr25_startgg_queries.transform.extract_event_phases import (
 logger = setup_logger(__name__)
 
 
-def refresh_raw_events(start_timestamp: int, end_timestamp: int):
+def refresh_raw_events():
+    sql = Postgres(
+        POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DB, POSTGRES_PORT
+    )
+    start_timestamp, end_timestamp = get_timestamps_for_refresh(sql)
     data = request_events(start_timestamp, end_timestamp)
     event_list, phase_list = extract_phase_and_event_from_response(data)
     event_df = pd.DataFrame(event_list)
     phase_df = pd.DataFrame(phase_list)
+    logger.info(
+        f"Obtained {len(event_df.tournament_name.unique())} different tournaments in this time period"
+    )
     event_df, phase_df = clean_event_and_phases_dataframes(event_df, phase_df)
     event_df["validated"] = False
     event_df["start_at"] = pd.to_datetime(event_df["start_at"], unit="s", utc=True)
     event_df["last_update_at"] = pd.to_datetime(
         event_df["last_update_at"], unit="s", utc=True
-    )
-    sql = Postgres(
-        POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DB, POSTGRES_PORT
     )
     sql.insert_values_to_table(event_df, "raw_events")
     sql.insert_values_to_table(phase_df, "raw_phases")
@@ -53,8 +56,4 @@ def refresh_raw_events(start_timestamp: int, end_timestamp: int):
 #     main()
 
 
-end_date = datetime.now()
-# start_date = end_date - timedelta(weeks=4)
-start_timestamp = 1733022000
-end_timestamp = int(end_date.timestamp())
-refresh_raw_events(start_timestamp, end_timestamp)
+refresh_raw_events()

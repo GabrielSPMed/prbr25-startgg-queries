c3216c102afb5e47fd20d957893bd15b53db88d3
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-09-28 20:57:01 -0300

    feat: add functions to extract event and phase data from timestamps and request data from GraphQL API

--- src/prbr25_startgg_queries/extract/refresh_raw.py
+++ src/prbr25_startgg_queries/extract/refresh_raw.py
@@ -1,9 +1,14 @@
 from datetime import datetime, timedelta
-from typing import Tuple
+from typing import List, Tuple
 
 from prbr25_startgg_queries.bd.postgres import Postgres
-from prbr25_startgg_queries.common.config import MAX_DATE_LIMIT
+from prbr25_startgg_queries.common.config import (
+    MAX_DATE_LIMIT,
+    STARTGG_BEARER_TOKEN,
+    events_dict,
+)
 from prbr25_startgg_queries.common.logger import setup_logger
+from prbr25_startgg_queries.extract.graphql import GraphQL
 from prbr25_startgg_queries.queries.sql_queries import most_recent_raw_event_query
 
 logger = setup_logger(__name__)
@@ -53,3 +58,43 @@ def fetch_most_recent_raw_event(sql: Postgres):
         f"The most recent tournament in the database is: {most_recent_event['tournament_name'][0]}: {most_recent_event['event_name'][0]} at {most_recent_event['start_at'][0]}"
     )
     return most_recent_event["start_at"][0]
+
+
+def extract_event_and_phase_dfs_from_timestamp(sql: Postgres):
+    """
+    Extracts raw event and phase dataframes from a given timestamp range.
+
+    This function retrieves the start and end timestamps for a refresh operation using the provided
+    Postgres connection, then requests event and phase data within that timestamp range.
+
+    Args:
+        sql (Postgres): An instance of the Postgres connection used to fetch timestamps.
+
+    Returns:
+        Any: Raw event and phase data retrieved for the specified timestamp range.
+    """
+    start_timestamp, end_timestamp = get_timestamps_for_refresh(sql)
+    raw_data = request_events_and_phases_from_timestamps(start_timestamp, end_timestamp)
+    return raw_data
+
+
+def request_events_and_phases_from_timestamps(start_date: int, end_date: int) -> List:
+    """
+    Fetches tournament event data within a specified date range.
+
+    Args:
+        start_date (int): The start date (timestamp or date integer) to filter events after.
+        end_date (int): The end date (timestamp or date integer) to filter events before.
+
+    Returns:
+        Any: Raw tournament event data retrieved from the GraphQL API.
+
+    Raises:
+        Exception: If the GraphQL query fails or returns an error.
+    """
+    events_dict["afterdate"] = start_date
+    events_dict["beforedate"] = end_date
+    raw_tournament_data = GraphQL(STARTGG_BEARER_TOKEN).query_all_pages_tournaments(
+        "events", events_dict
+    )
+    return raw_tournament_data

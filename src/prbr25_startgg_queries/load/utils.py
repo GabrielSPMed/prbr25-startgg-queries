0e9bb4c816a9760015ca98a4554cfe296bac9bc8
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-09-28 20:57:18 -0300

    feat: add function to load pandas DataFrames into PostgreSQL

---
+++ src/prbr25_startgg_queries/load/utils.py
@@ -0,0 +1,21 @@
+from typing import Dict
+
+from prbr25_startgg_queries.bd.postgres import Postgres
+
+
+def load_pandas_dataframes_into_postgres(sql: Postgres, dataframes_dict: Dict):
+    """
+    Loads multiple pandas DataFrames into corresponding PostgreSQL tables.
+
+    Args:
+        sql (Postgres): An instance of the Postgres class used to interact with the database.
+        dataframes_dict (Dict): A dictionary where keys are table names (str) and values are pandas DataFrames to be inserted.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: Propagates any exceptions raised during the insertion process.
+    """
+    for table_name, df in dataframes_dict.items():
+        sql.insert_values_to_table(df, table_name)

209dfd86f6b9f9c16097f0bf4329213191b9df64
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-11-20 17:10:26 -0300

    feat: add get_update_query function to generate SQL update statements

--- src/prbr25_startgg_queries/queries/sql_queries.py
+++ src/prbr25_startgg_queries/queries/sql_queries.py
@@ -4,3 +4,12 @@ most_recent_raw_event_query = """
         ORDER BY start_at DESC
         LIMIT 1
     """
+
+
+def get_update_query(table_name: str, column_name: str, ids: str, value) -> str:
+    query = f"""
+            UPDATE {table_name}
+            SET {column_name} = {value}
+            WHERE id IN ({ids})
+        """
+    return query
